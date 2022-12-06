f3b8c9bf14eaf6c6ac6d394e515968172a5d0f8e
ruthie <doodlepath@gmail.com>
2022-12-06 07:55:18 -0600

    Day 6

--- Day6/Day6a.py
+++ Day6/Day6a.py
@@ -1,9 +1,21 @@
-sections = 0
 
-f = open("Day6/day_6_test_input.txt", "r")
-lines = f.readlines()
-for line in lines: 
-    print(line)
+count = 3
 
-print(f"The total value is {sections}")
+f = open("Day6/day_6_input.txt", "r")
+line = f.readlines()
+stream = list(line[0])
+num_characters = len(stream)
+print('stream', stream)
+while count < num_characters : 
+
+    one = stream[count-3]
+    two = stream[count-2]
+    three = stream[count-1]
+    four = stream[count]
+    if one != two and one != three and one != four and two != three and two != four and three != four and count >= 4: 
+        print(f"{count+1} chars need to be processed to get through the start-of-packet marker.")
+        print(one, two, three, four)
+        break
+    else : count+=1
+  
 
--- Day6/Day6b.py
+++ Day6/Day6b.py
@@ -1,9 +1,31 @@
-sections = 0
+dict = {}
+count = 0
 
-f = open("Day6/day_6_test_input.txt", "r")
-lines = f.readlines()
-for line in lines: 
-    print(line)
+f = open("Day6/day_6_input.txt", "r")
+line = f.readlines()
+stream = list(line[0])
+num_characters = len(stream)
+print('stream', stream)
+while count < num_characters : 
+    char = stream[count]
+    if char in dict : dict[char].append(count)
+    else : dict[char] = [count]
 
-print(f"The total value is {sections}")
+    if count > 13 : 
+        cut = count - 14
+        for indecis in dict.values() : 
+            if indecis.count(cut) : 
+                indecis.remove(cut)
+    check_for_deletion = ''
+    for key in dict : 
+        if len(dict[key]) == 0 : check_for_deletion = key
+    if check_for_deletion != '' : 
+        dict.pop(check_for_deletion, None)
+    if len(dict) == 14 : 
+       print(f"{count+1} chars need to be processed to get through the start-of-message marker.")
+       break
+    count+=1
+ 
 
+
+  

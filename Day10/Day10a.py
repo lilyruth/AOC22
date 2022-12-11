652c8d4130035825b419e89400407a01bf0c23b3
ruthie <doodlepath@gmail.com>
2022-12-10 20:29:12 -0600

    Day 10 done yaaaaaaaaaay

--- Day10/Day10a.py
+++ Day10/Day10a.py
@@ -1,9 +1,30 @@
-sections = 0
+cycle = 1
+x = 1
+cycles = []
 
-f = open("Day6/day_6_test_input.txt", "r")
+f = open("Day10/day_10_input.txt", "r")
 lines = f.readlines()
 for line in lines: 
-    print(line)
+    line = line.rstrip('\n')
+    line = line.split(' ')
+    if line[0] == 'noop': 
+        if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
+            cycles.append(x * cycle)
+            print(f"The cycle is {cycle} and x is {x}")
+        cycle+=1 
+    else: 
+        add_val = int(line[1])
+        if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
+            cycles.append(x * cycle)
+            print(f"The cycle is {cycle} and x is {x}")
+        cycle += 1
+        if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
+            cycles.append(x * cycle)
+            print(f"The cycle is {cycle} and x is {x}")  
+        cycle += 1  
+        x += add_val
+    print(f"The cycle is {cycle} and x is {x}")
+    
 
-print(f"The total value is {sections}")
+print(f"The list is {cycles} and total value is {sum(cycles)}")
 
--- Day10/Day10b.py
+++ Day10/Day10b.py
@@ -1,9 +1,59 @@
-sections = 0
+cycle = 1
+current_draw = (cycle - 1) % 40
+x = 1
 
-f = open("Day6/day_6_test_input.txt", "r")
+current_sprite_1 = 0
+current_sprite_2 = 1
+current_sprite_3 = 2
+
+count = 0
+rows = 0
+CRT = []
+
+while len(CRT) < 6:
+    row = []
+    while len(row) < 40:
+        row.append(' ')
+    CRT.append(row)
+
+f = open("Day10/day_10_input.txt", "r")
 lines = f.readlines()
 for line in lines: 
-    print(line)
+    line = line.rstrip('\n')
+    line = line.split(' ')
 
-print(f"The total value is {sections}")
+    if current_sprite_1 == current_draw or current_sprite_2 == current_draw or current_sprite_3 == current_draw:
+        row = int(cycle / 40)
+        col = (cycle - 1) % 40
+        CRT[row][col] = '#'
 
+    if line[0] == 'noop': 
+        cycle+=1
+        current_draw = (cycle - 1) % 40 
+
+    else: 
+
+        if current_sprite_1 == current_draw or current_sprite_2 == current_draw or current_sprite_3 == current_draw:
+            row = int(cycle / 40)
+            col = (cycle - 1) % 40
+            CRT[row][col] = '#'
+
+        add_val = int(line[1])
+        cycle += 1
+        current_draw = (cycle - 1) % 40 
+
+        if current_sprite_1 == current_draw or current_sprite_2 == current_draw or current_sprite_3 == current_draw:
+            row = int(cycle / 40)
+            col = (cycle - 1) % 40
+            CRT[row][col] = '#'
+        
+        cycle += 1  
+        current_draw = (cycle - 1) % 40 
+
+        x += add_val
+        current_sprite_1 = x - 1
+        current_sprite_2 = x 
+        current_sprite_3 = x + 1
+
+print(CRT)
+    
